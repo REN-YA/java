a7830103783cc0de9e4bdb13333ebc0042023b40
REN-YA <95633289+REN-YA@users.noreply.github.com>
2022-05-13 19:13:16 +0900

    study

--- Ready02/src/study02.java
+++ Ready02/src/study02.java
@@ -16,21 +16,36 @@ public class study02 {
 //		int x = 13+17;
 //		System.out.println("x="x);
 		//数値 13 と 17 の積を表示するプログラムを作成しなさい。ただし、変数を使用しないこと。
-		int x = 13*17;
-		System.out.println("x="+x);
+//		int x = 13*17;
+//		System.out.println("x="+x);
 		//次のプログラムを作成しなさい。
 			//int 型の変数 x に数値 7 を代入する。
 			//変数 x の値を 3 倍にする。
 			//変数 x の値を表示する。
 			//変数 x の値を半分（1/2）にする。
 			//変数 x の値を表示する。
-		
+//			int x = 7;
+//			System.out.println(x*3);
+//			System.out.println((double)x/2);
 		//int 型の変数 x、y に任意の数値を代入し、x の値を y に、y の値を x に入れ替えて x と y の値を表示するプログラムを作成しなさい。
+//		int x = 4;
+//		int y = 5;
+//		int z = x;
+//		x = y;
+//		y = z;
+//		System.out.println(x);
+//		System.out.println(y);
 		
 		//int 型の変数 x、y に数値 19、23 を代入し、その積を変数 z に代入して z の値を表示するプログラムを作成しなさい。
-		
+//		int x = 19;
+//		int y = 23;
+//		int z = x*y;
+//		System.out.println(z);
 		//int 型の変数 x に任意の数値を代入し、x を 2 倍、3 倍、4 倍した結果を表示するプログラムを作成しなさい。
-		
+//		int x = 2;
+//		System.out.println(x*2);
+//		System.out.println(x*3);
+//		System.out.println(x*4);
 		//int 型の変数 x に任意の数値を代入し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
 		
 		//int 型の変数 x に任意の数値を代入し、x を x より小さい任意の数値で割った商と余りを表示するプログラムを作成しなさい。
