8975f6f97219258d61a30583de21bce3dd5fe55d
REN-YA <95633289+REN-YA@users.noreply.github.com>
2022-05-15 16:49:41 +0900

    study

--- Ready02/src/study03.java
+++ Ready02/src/study03.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 public class study03 {
 	public static void main(String[] args)throws IOException {
 //		 BufferedReader br = new BufferedReader(
@@ -32,6 +34,16 @@ public class study03 {
 //		System.out.println("商 " + ( x / y ) );
 //		System.out.println("余り " + ( x % y ) );
 		//２つの整数値を入力し、平均値を求めるプログラムを作成しなさい。
+//		BufferedReader br = new BufferedReader(
+//				new InputStreamReader(System.in));
+//		int x = Integer.parseInt(br.readLine());
+//		int y = Integer.parseInt(br.readLine());
+//		System.out.println( "平均値=" + ( ( x + y ) / 2 ) );
 		//年齢を訊ね、生まれてから現在までの、おおよその日数を表示するプログラムを作成しなさい。
+		BufferedReader br = new BufferedReader(
+                new InputStreamReader( System.in ) );
+		System.out.println( "あなたの年齢は？" );
+		int age = Integer.parseInt( br.readLine() );
+		System.out.println( "生まれてから今まで、およそ " + ( age * 365 ) + "日です" );
 	}
 }
