8bd42ab818e54745a7c749d305aee5bfd61caa7b
REN-YA <95633289+REN-YA@users.noreply.github.com>
2022-05-27 14:05:20 +0900

    study

--- Ready02/src/study03.java
+++ Ready02/src/study03.java
@@ -9,17 +9,26 @@ public class study03 {
 //		 String  s = br.readLine();    // 1行分の文字列を入力する
 		
 		//String 型の変数 s に文字列を入力し、s の値を表示するプログラムを作成しなさい。
-		BufferedReader br = new BufferedReader(
-					new InputStreamReader(System.in));
-
-		String s = br.readLine();
-		System.out.println(s);
+//		BufferedReader br = new BufferedReader(
+//					new InputStreamReader(System.in));
+//
+//		String s = br.readLine();
+//		System.out.println(s);
 		
 		//int 型の変数 x に数値を入力し、x の値を表示するプログラムを作成しなさい。
-		
+//		BufferedReader br = new BufferedReader(
+//					new InputStreamReader(System.in));
+//		
+//		int x = Integer.parseInt(br.readLine());
+//		System.out.println(x);
 
 		//int 型の変数 x に数値を入力し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
-
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		System.out.println(x);
+		System.out.println(x*x);
+		System.out.println(x*x*x);
 		
 		//int 型の変数 x、y にそれぞれ数値を入力し、x と y の和、差（x－y）、積、商と余り(x÷y)、を表示するプログラムを作成しなさい。
 //		
