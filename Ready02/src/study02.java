ed00c25cda243acafd53cf62c1efb426daea1896
REN-YA <95633289+REN-YA@users.noreply.github.com>
2022-06-16 17:10:00 +0900

    study

--- Ready02/src/study02.java
+++ Ready02/src/study02.java
@@ -30,23 +30,31 @@ public class study02 {
 //		System.out.println(x);
 			
 		//int 型の変数 x、y に任意の数値を代入し、x の値を y に、y の値を x に入れ替えて x と y の値を表示するプログラムを作成しなさい。
-		int x = 2;
-		int y = 4;
-		int z = y;
-		y = x;
-		x = z;
-		System.out.println(x);
-		System.out.println(y);
+//		int x = 2;
+//		int y = 4;
+//		int z = y;
+//		y = x;
+//		x = z;
+//		System.out.println(x);
+//		System.out.println(y);
 		
 		//int 型の変数 x、y に数値 19、23 を代入し、その積を変数 z に代入して z の値を表示するプログラムを作成しなさい。
-	        
+//	    int x = 19;
+//	    int y = 23;
+//	    int z = x*y;
+//	    System.out.println(z);
 	        
 		//int 型の変数 x に任意の数値を代入し、x を 2 倍、3 倍、4 倍した結果を表示するプログラムを作成しなさい。			
-		
+//		int x = 2;
+//		System.out.println(x*2);
+//		System.out.println(x*3);
+//		System.out.println(x*4);
 		
 		//int 型の変数 x に任意の数値を代入し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
-			
-		
+		int x = 3;
+		System.out.println(x);
+		System.out.println(x*x);
+		System.out.println(x*x*x);
 		
 		//int 型の変数 x に任意の数値を代入し、x を x より小さい任意の数値で割った商と余りを表示するプログラムを作成しなさい。
 			
