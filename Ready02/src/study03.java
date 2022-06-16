ea0a2a00f37fc873824301cd0e3ab40964f7d891
REN-YA <95633289+REN-YA@users.noreply.github.com>
2022-06-16 16:53:07 +0900

    study

--- Ready02/src/study03.java
+++ Ready02/src/study03.java
@@ -1,7 +1,5 @@
 //【練習問題】入力
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 public class study03 {
 	public static void main(String[] args)throws IOException {
 //		 BufferedReader br = new BufferedReader(
@@ -9,51 +7,22 @@ public class study03 {
 //		 String  s = br.readLine();    // 1行分の文字列を入力する
 		
 		//String 型の変数 s に文字列を入力し、s の値を表示するプログラムを作成しなさい。
-//		BufferedReader br = new BufferedReader(
-//					new InputStreamReader(System.in));
-//
-//		String s = br.readLine();
-//		System.out.println(s);
+
 		
 		//int 型の変数 x に数値を入力し、x の値を表示するプログラムを作成しなさい。
-//		BufferedReader br = new BufferedReader(
-//					new InputStreamReader(System.in));
-//		
-//		int x = Integer.parseInt(br.readLine());
-//		System.out.println(x);
+
 
 		//int 型の変数 x に数値を入力し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
-//		BufferedReader br = new BufferedReader(
-//				new InputStreamReader(System.in));
-//		int x = Integer.parseInt(br.readLine());
-//		System.out.println(x);
-//		System.out.println(x*x);
-//		System.out.println(x*x*x);
+
 		
 		//int 型の変数 x、y にそれぞれ数値を入力し、x と y の和、差（x－y）、積、商と余り(x÷y)、を表示するプログラムを作成しなさい。
-//		BufferedReader br = new BufferedReader(
-//					new InputStreamReader(System.in));
-//		int x = Integer.parseInt(br.readLine());
-//		int y = Integer.parseInt(br.readLine());
-//		System.out.println(x+y);
-//		System.out.println(x-y);
-//		System.out.println(x*y);
-//		System.out.println(x/y);
-//		System.out.println(x%y);
-//		
+
 		
 		//２つの整数値を入力し、平均値を求めるプログラムを作成しなさい。
-//		BufferedReader br = new BufferedReader(
-//				new InputStreamReader(System.in));
-//		int x = Integer.parseInt(br.readLine());
-//		int y = Integer.parseInt(br.readLine());
-//		System.out.println((x+y)/2);
+
 		
 		//年齢を訊ね、生まれてから現在までの、おおよその日数を表示するプログラムを作成しなさい。
-		BufferedReader br = new BufferedReader(
-				new InputStreamReader(System.in));
-		int x = Integer.parseInt(br.readLine());
-		System.out.println(x*356);
+		
 		
 	}
 }
