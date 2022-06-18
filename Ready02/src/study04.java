de5d1dd15d395c710a8c60b092c00787391227f4
REN-YA <95633289+REN-YA@users.noreply.github.com>
2022-06-18 17:48:04 +0900

    study

--- Ready02/src/study04.java
+++ Ready02/src/study04.java
@@ -135,38 +135,92 @@ public class study04 {
 //		System.out.println( "不合格" );
 //				
 		//曜日と、午前、午後、夜間の区別を入力し、病院が開いているか、休診であるかを表示するプログラムを作成しなさい。
+//		BufferedReader br = new BufferedReader(
+//                new InputStreamReader( System.in ) );
+//
+//		System.out.println( "曜日を選択してください" );
+//		System.out.print( "0=日曜、1=月曜、2=火曜、3=水曜、4=木曜、5=金曜、6=土曜" );
+//		int day = Integer.parseInt( br.readLine() );
+//		
+//		System.out.println( "時間帯を選択してください" );
+//		System.out.print( "0=午前、1=午後、2=夜間" );
+//		int zone = Integer.parseInt( br.readLine() );
+//		
+//		boolean bopen = true;
+//		
+//		if( day == 0 )
+//		bopen = false;
+//		else if( zone == 0 && ( day == 2 || day == 5 ) )
+//		bopen = false;
+//		else if( zone == 1 && day == 6 )
+//		bopen = false;
+//		else if( zone == 2 && ( day == 3 || day == 6 ) )
+//		bopen = false;
+//		
+//		if( bopen )
+//		System.out.println( "診療しています。" );
+//		else
+//		System.out.println( "休診です。" );
+		
+		//整数値 x, y を入力し、以下の条件に該当する場合、そうであることを示す文を表示しなさい。
 		BufferedReader br = new BufferedReader(
                 new InputStreamReader( System.in ) );
 
-		System.out.println( "曜日を選択してください" );
-		System.out.print( "0=日曜、1=月曜、2=火曜、3=水曜、4=木曜、5=金曜、6=土曜" );
-		int day = Integer.parseInt( br.readLine() );
+		int x = Integer.parseInt( br.readLine() );
+		int y = Integer.parseInt( br.readLine() );
 		
-		System.out.println( "時間帯を選択してください" );
-		System.out.print( "0=午前、1=午後、2=夜間" );
-		int zone = Integer.parseInt( br.readLine() );
+		if( x < y && ( x % 2 ) == 0 && ( y % 2 ) == 0 )
+		System.out.println( "xはyより小さく、かつ、xとyは共に偶数である。" );
 		
-		boolean bopen = true;
+		if( x == y && x < 0 )
+		System.out.println( "xとyは等しく、かつ、負の数である。" );
 		
-		if( day == 0 )
-		bopen = false;
-		else if( zone == 0 && ( day == 2 || day == 5 ) )
-		bopen = false;
-		else if( zone == 1 && day == 6 )
-		bopen = false;
-		else if( zone == 2 && ( day == 3 || day == 6 ) )
-		bopen = false;
+		if( x < y || ( x % 2 ) == 0 )
+		System.out.println( "xはyより小さい、または、xは偶数である。" );
 		
-		if( bopen )
-		System.out.println( "診療しています。" );
-		else
-		System.out.println( "休診です。" );
+		if( ( x <= 10 || x >= 100 ) && ( y >= 10 && y <= 100 ) )
+		System.out.println( "xは10以下または100以上で、かつ、yは10以上かつ100以下である。" );
 		
-		//整数値 x, y を入力し、以下の条件に該当する場合、そうであることを示す文を表示しなさい。
-
+		if( ! ( x < 0 && y < 0 ) )
+		System.out.println( "xもyも負の数である、ではない。" );
 //				
 		//月を表す数値を入力し、その月の初めから年末までにある祝日を表示するプログラムを作成しなさい。
+		BufferedReader br = new BufferedReader(
+                new InputStreamReader( System.in ) );
 
+		System.out.println( "何月ですか？" );
+		int month = Integer.parseInt( br.readLine() );
+		
+		switch( month )
+		{
+		case 1:
+		System.out.println("元日" );
+		System.out.println("成人の日");
+		case 2:
+		System.out.println("建国記念の日");
+		case 3:
+		System.out.println("春分の日");
+		case 4:
+		System.out.println("昭和の日");
+		case 5:
+		System.out.println("憲法記念日");
+		System.out.println("みどりの日");
+		System.out.println("こどもの日");
+		case 6:
+		case 7:
+		System.out.println("海の日");
+		case 8:
+		case 9:
+		System.out.println("敬老の日");
+		System.out.println("秋分の日");
+		case 10:
+		System.out.println("体育の日");
+		case 11:
+		System.out.println("文化の日" );
+		System.out.println("勤労感謝の日");
+		case 12:
+		System.out.println("天皇誕生日");
+		}
 //		
 		//好きな鮨（すし）を選択させ、それに対応したメッセージを表示する、鮨占いプログラムを作成しなさい。
 
